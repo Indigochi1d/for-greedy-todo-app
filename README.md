59be293eab6d543174b72ad03b276df07dad5944
indigo <diego97523@gmail.com>
2024-10-02 21:44:58 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+<h1>Todo-app</h1>
+<hr>
+<p>할일 입력</p>
+<p>삭제</p>
+<p>체크기능</p>
+<hr>
+<img width="800" alt="스크린샷 2023-09-13 오후 7 56 44" src="https://github.com/indigchi1d/React_Study/assets/33516980/be1d75b1-415a-41af-a486-652e14d7522f">
---
+++ src/App.js
@@ -0,0 +1,42 @@
+import logo from './logo.svg';
+import './App.css';
+import TodoTemplate from './components/TodoTemplate';
+import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
+import { useRef, useState } from 'react';
+
+function App() {
+	const [todos, setTodos] = useState([]); // 초기값을 빈 배열로 설정
+	const nextId = useRef(1); // nextId를 1로 초기화
+	
+	const onInsert = (text) => {
+		const todo = {
+			id: nextId.current,
+			text,
+			checked: false,
+		};
+		setTodos(todos.concat(todo));  
+		nextId.current += 1;
+	};
+
+	const onRemove = (id) => {
+		setTodos(todos.filter((todo) => todo.id !== id));
+	};
+
+	const onToggle = (id) => {
+		setTodos(todos.map((todo) =>
+			todo.id === id ? { ...todo, checked: !todo.checked } : todo
+		));
+	};
+
+	return (
+		<>
+			<TodoTemplate>
+				<TodoInsert onInsert={onInsert} />
+				<TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
+			</TodoTemplate>
+		</>
+	);
+}
+
+export default App;
