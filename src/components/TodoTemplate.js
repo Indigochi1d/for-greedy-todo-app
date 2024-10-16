ceb7feb9aa2be1e24cf5b395b32673deeca738b9
indigo <diego97523@gmail.com>
2024-10-16 15:44:10 +0900

    chore: add greedy logo

--- src/components/TodoTemplate.js
+++ src/components/TodoTemplate.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './css/TodoTemplate.scss';
+import GreedyLogo from "../greedy_logo.jpeg"
 const TodoTemplate = ( {children} ) => {
     return (
         <div className='TodoTemplate'>
-            <div className='app-title'>일정관리</div>
-            <div className='content'>{children}</div>
+          <div className="app-title">
+            <img src={GreedyLogo} alt="greedyLogo" className="greedyLogo" />
+            <p>일정관리</p>
+          </div>
+          <div className="content">{children}</div>
         </div>
     );
 };
